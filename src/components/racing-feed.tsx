6e4e61e660994285a399b4c7e7e8cb99001c210d
kristojorg <kristo.jorgenson@gmail.com>
2021-05-04 19:26:16 -0400

    add time

--- src/components/racing-feed.tsx
+++ src/components/racing-feed.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
-import { Box } from '@chakra-ui/react';
+import { VStack } from '@chakra-ui/react';
 import videos from '../assets/videos.json';
 import VideoItem from './video-item';
 
 export default function RacingFeed() {
   return (
-    <Box>
+    <VStack spacing={10}>
       {videos.map((video) => (
         <VideoItem video={video} key={video.urlTitle} />
       ))}
-    </Box>
+    </VStack>
   );
 }
--- src/components/video-item.tsx
+++ src/components/video-item.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
-import { Box, Image, Text } from '@chakra-ui/react';
+import { Box, Heading, Image, Link, Text } from '@chakra-ui/react';
 import videos from '../assets/videos.json';
 import { getYTPreviewUrl } from '../utils';
+import moment from 'moment';
 
 type VideoItemProps = {
   video: typeof videos[0];
@@ -11,7 +12,14 @@ const VideoItem: React.FC<VideoItemProps> = ({ video }) => {
   return (
     <Box>
       <Image src={getYTPreviewUrl(video._id)} alt={video.title} width="100%" />
-      <Text>{video.title}</Text>
+      <Link>
+        <Heading pt={2} size="md">
+          {video.title}
+        </Heading>
+      </Link>
+      <Box as="time" dateTime={video.publishDate} fontSize="xs" mt={1}>
+        {moment(video.publishDate).fromNow()}
+      </Box>
     </Box>
   );
 };
