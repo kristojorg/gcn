07e04105ea1c1b13c490c1f9eb92dbfc5412078e
kristojorg <kristo.jorgenson@gmail.com>
2021-05-04 19:32:36 -0400

    add link

--- src/components/video-item.tsx
+++ src/components/video-item.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
-import { Box, Heading, Image, Link, Text } from '@chakra-ui/react';
+import { Box, Heading, Image, LinkBox, LinkOverlay } from '@chakra-ui/react';
 import videos from '../assets/videos.json';
 import { getYTPreviewUrl } from '../utils';
 import moment from 'moment';
+import NextLink from 'next/link';
 
 type VideoItemProps = {
   video: typeof videos[0];
@@ -10,17 +11,19 @@ type VideoItemProps = {
 
 const VideoItem: React.FC<VideoItemProps> = ({ video }) => {
   return (
-    <Box>
+    <LinkBox>
       <Image src={getYTPreviewUrl(video._id)} alt={video.title} width="100%" />
-      <Link>
-        <Heading pt={2} size="md">
-          {video.title}
-        </Heading>
-      </Link>
+      <NextLink href={`/videos/${video.urlTitle}`}>
+        <LinkOverlay>
+          <Heading pt={2} size="md">
+            {video.title}
+          </Heading>
+        </LinkOverlay>
+      </NextLink>
       <Box as="time" dateTime={video.publishDate} fontSize="xs" mt={1}>
         {moment(video.publishDate).fromNow()}
       </Box>
-    </Box>
+    </LinkBox>
   );
 };
 
