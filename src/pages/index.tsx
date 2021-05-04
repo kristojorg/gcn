16a8b61fe318296a06b4eec4e52cf359dc519e5b
kristojorg <kristo.jorgenson@gmail.com>
2021-05-04 17:02:57 -0400

    init typescript and prettier

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>GCN | Home</title>
+        <meta name="description" content="The source for cycling contnet." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>hello world</h1>
+    </div>
+  );
+}
