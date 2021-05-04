4ce42cce967d432c360d9d3394fc2a0e20e83b68
kristojorg <kristo.jorgenson@gmail.com>
2021-05-04 17:06:21 -0400

    ts update

---
+++ src/constants.ts
@@ -0,0 +1,1 @@
+export const GCN_ENDPOINT = 'https://globalcyclingnetwork.com/api/devtask.';
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
-import { ChakraProvider } from "@chakra-ui/react"
+import * as React from 'react';
+import type { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <Component {...pageProps} />
     </ChakraProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
